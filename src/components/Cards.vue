50acdc713fb6e597633dac2c2b430b037d885657
Roeen <roeenacal@gmail.com>
2023-08-18 21:55:48 -0700

    Add InterserctionObserver to stop unnecessary HTTP requests after reaching the end of the array

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -4,11 +4,15 @@ import { onMounted, ref } from 'vue';
 import { useStoreUser } from '../stores/storeUser';
 import { supabase } from '../supabase';
 import Card from './Card.vue';
+import Observer from './Observer.vue';
 
 const store = useStoreUser()
 const { user } = storeToRefs(store)
 
 const posts = ref([])
+const ownerIds = ref([])
+const lastCardIndex = ref(2)
+const reachedEnd = ref(false)
 
 async function fetchData() {
   // fetch the IDs of the users that you are following
@@ -17,16 +21,41 @@ async function fetchData() {
     .select('following_id')
     .eq('follower_id', user.value.id )
 
-  const owner_ids = followings.map(f => f.following_id)
+    ownerIds.value = followings.map(f => f.following_id)
   
   // fetch posts from the users you are following
   const { data: postsData } = await supabase
     .from('posts')
     .select()
-    .in('owner_id', owner_ids)
-    .order('created_at',{ ascending: false }) //fetch latest
+    .in('owner_id', ownerIds.value)
+    .range(0,lastCardIndex.value) //only fetch 3
+    .order('created_at',{ ascending: false }) //fetch latest posts
 
-  posts.value = postsData
+    posts.value = postsData
+}
+
+async function fetchNextSet() {
+  if(!reachedEnd.value) {
+    const { data: postsData } = await supabase
+    .from('posts')
+    .select()
+    .in('owner_id', ownerIds.value)
+    .range(lastCardIndex.value + 1,lastCardIndex.value + 3) //fetch next 3
+    .order('created_at',{ ascending: false }) //fetch latest posts
+
+    //update posts.value
+    posts.value = [
+      ...posts.value,
+      ...postsData
+    ]
+
+    //prevent from making unnecessary HTTP requests after reaching the end of the array
+    lastCardIndex.value = lastCardIndex.value + 3
+
+    if(!postsData.length) {
+      reachedEnd.value = true
+    }
+  }
 }
 
 onMounted(() => fetchData())
@@ -40,6 +69,7 @@ onMounted(() => fetchData())
       :key="post.id"
       :post="post"
     />
+    <Observer v-if="posts.length" @intersect="fetchNextSet" />
   </div>
 </template>
 
---
+++ src/components/Observer.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { onMounted, ref} from 'vue';
+
+
+const observer = ref(null)
+const root = ref(null)
+
+const emits = defineEmits(['intersect'])
+
+onMounted(() => {
+  observer.value = new IntersectionObserver(([entry]) => {
+    if(entry && entry.isIntersecting) {
+      emits('intersect') //emit to parent component -> Cards
+    }
+  })
+
+  observer.value.observe(root.value)
+
+})
+</script>
+
+<template>
+  <div ref="root" class="observer">
+
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.observer {
+  height: 10px;
+  width: 10px;
+}
+</style>
